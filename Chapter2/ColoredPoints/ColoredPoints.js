28964f57903eab144b3fef2ffb11754a2e49bf2f
ChallengerCY <790597004@qq.com>
2019-11-21 18:00:24 +0800

    练习根据点击事件使用WebGL绘制不同颜色的点

---
+++ Chapter2/ColoredPoints/ColoredPoints.js
@@ -0,0 +1,85 @@
+let VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    'gl_Position=a_Position;\n' +
+    'gl_PointSize=10.0;\n' +
+    '}\n';
+
+let FSHADER_SOURCE =
+    'precision mediump float;\n'+
+    'uniform vec4 u_FragColor;\n'+
+    'void main() {\n' +
+    'gl_FragColor=u_FragColor;\n' +
+    '}\n';
+
+let g_points = [];
+let g_colors = [];
+
+function main(){
+    let canvas = document.getElementById('webgl');
+
+    let gl = getWebGLContext(canvas);
+
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log("Failed to initialize shaders.");
+        return;
+    }
+
+    let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Fialed to get the storage location of a a_Position');
+        return;
+    }
+
+    let  u_FragColor=gl.getUniformLocation(gl.program,'u_FragColor');
+    if (u_FragColor < 0) {
+        console.log('Fialed to get the storage location of a u_FragColor');
+        return;
+    }
+    canvas.onmousedown = function (ev) {
+        click(ev, gl, canvas, a_Position,u_FragColor);
+    };
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+/**
+ * 响应鼠标点击事件
+ * @param ev 点击参数回调
+ * @param gl WebGL对象
+ * @param canvas 画布
+ * @param a_Position attribute对象的地址(顶点的位置)
+ * @param u_FragColor uniform对象的地址(顶点的颜色)
+ */
+function click(ev, gl, canvas, a_Position,u_FragColor) {
+    let x = ev.clientX;
+    let y = ev.clientY;
+    let rect = ev.target.getBoundingClientRect();
+    x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
+    y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+
+    g_points.push({x, y});
+    if (x >= 0.0 && y > 0.0) {
+        g_colors.push([1.0, 0.0, 0.0, 1.0]);
+    } else if (x < 0.0 && y < 0.0) {
+        g_colors.push([0.0, 1.0, 0.0, 1.0]);
+    } else
+    {
+        g_colors.push([1.0,1.0,1.0,1.0]);
+    }
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    let len = g_points.length;
+    for (let i = 0; i < len; i++) {
+        gl.vertexAttrib3f(a_Position, g_points[i].x, g_points[i].y, 0.0);
+        gl.uniform4fv(u_FragColor,g_colors[i]);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+
+}
