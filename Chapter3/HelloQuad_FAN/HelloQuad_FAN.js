a5ebab15f3b8344306d2f27555470f6e5847b90f
ChallengerCY <790597004@qq.com>
2019-11-27 11:54:05 +0800

    练习绘制四边形和不规则图形

---
+++ Chapter3/HelloQuad_FAN/HelloQuad_FAN.js
@@ -0,0 +1,56 @@
+let VSHADER_SOURCE=
+    'attribute vec4 a_Position;\n'+
+    'void main(){\n'+
+    'gl_Position=a_Position;\n'+
+    '}\n';
+
+let  FSHADER_SOURCE=
+    'void main(){\n'+
+    'gl_FragColor=vec4(1.0,0.0,0.0,1.0);\n'+
+    '}\n';
+
+function main() {
+
+    let canvas = document.getElementById('webgl');
+    let gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log();
+        return;
+    }
+
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE))
+    {
+        return;
+    }
+
+    let n=InitVertexBuffers(gl);
+
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_FAN,0,n);
+}
+
+function InitVertexBuffers(gl) {
+
+    let vertex=new Float32Array([-0.5,0.5,-0.5,-0.5,0.5,0.5,0.5,-0.5]);
+
+    let  n=4;
+
+    let vertexBuffer=gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER,vertex,gl.STATIC_DRAW);
+
+    let a_Position=gl.getAttribLocation(gl.program,'a_Position');
+
+    gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,0,0);
+
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
+
+/** 本段代码小结
+ * 使用gl.TRIANGLE_FAN绘制一个飘带形状。
+ */
